b9adc6f6a6b8ccb0b6ea214f7a79cba8f91dc049
Jun Quan <147310480+jquan18@users.noreply.github.com>
2024-03-31 01:23:35 +0800

    Add files via upload

--- ShariahChecker4.py
+++ ShariahChecker4.py
@@ -4,37 +4,18 @@ import openai
 import os
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import re
 
-# Set your OpenAI API key
-openai.api_key = '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'
+openai.api_key = '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'
 
 def save_uploaded_file(uploaded_file, folder_path, file_name):
-    # Create the uploads directory if it doesn't exist
+    # Create the uploads directory if it doesn't exists
     os.makedirs(folder_path, exist_ok=True)
     # Save the file to the uploads directory
     file_path = os.path.join(folder_path, file_name)
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     return file_path
-
-# def extract_pages_with_keywords(input_pdf_path, keywords):
-#     # Open the PDF file
-#     with open(input_pdf_path, "rb") as file:
-#         # Create a PdfReader object
-#         pdf_reader = PyPDF2.PdfReader(file)
-#         # Initialize an empty string to store the extracted pages
-#         extracted_pages_text = ""
-#
-#         # Iterate through each page of the PDF
-#         for page_number, page in enumerate(pdf_reader.pages, start=1):
-#             # Extract text from the page
-#             page_text = page.extract_text()
-#             # Check if any of the keywords are present in the page text
-#             if any(keyword in page_text for keyword in keywords):
-#                 # Append the page text to the extracted pages string
-#                 extracted_pages_text += page_text
-#
-#         return extracted_pages_text
 
 def extract_pages_with_keywords(pdf_file, keywords):
     # Create a PdfReader object directly from the file object
@@ -74,7 +55,7 @@ def extract_total_cash(text):
         model="ft:gpt-3.5-turbo-0125:personal::98TxxR5J",  # You can use any appropriate GPT model here
         messages=[
             {"role": "system",
-             "content": "Find all figures in group/consolidated section for the latest year that contains the word 'cash' and add them together ,  use group/consolidated, don't include cash in company):"},
+             "content": "Find all figures in group/consolidated section for the latest year that contains the word 'cash' and add them together ,  use group/consolidated, don't include cash in company, also DON'T DISPLAY prefix such as million or RM'000, and make sure to give me the sum if there are more than one cash related figures):"},
             {"role": "user", "content": text},
         ],
         max_tokens=4000,
@@ -88,7 +69,7 @@ def extract_total_debt(text):
         model="ft:gpt-3.5-turbo-0125:personal::98TxxR5J",  # You can use any appropriate GPT model here
         messages=[
             {"role": "system",
-             "content": "Find all figures in group/consolidated section for the latest year that contains the word 'borrowing' and 'loan'(sum up short term and long term) and  add them together( please don't add any borrowings or loan under company! I only want group!) ):, please use prefix at the right place"},
+             "content": "Find all figures in group/consolidated section for the latest year that contains the word 'borrowing' and 'loan'(sum up short term and long term) and  add them together( please don't add any borrowings or loan under company! I only want group!,also don't take into account prefix such as million or RM'000) ):"},
             {"role": "user", "content": text},
         ],
         max_tokens=4000,
@@ -103,7 +84,7 @@ def extract_total_assets(text):
         model="ft:gpt-3.5-turbo-0125:personal::98TxxR5J",  # You can use any appropriate GPT model here
         messages=[
             {"role": "system",
-             "content": "Extract the total Asset for group/consolidated for the latest year from the provided text"},
+             "content": "Extract the total Asset for group/consolidated for the latest year from the provided text, also don't take into account prefix such as million or RM'000"},
             {"role": "user", "content": text},
         ],
         max_tokens=4000,
@@ -125,8 +106,6 @@ def extract_total_assets(text):
 #     )
 #     floated = float((response['choices'][0]['message']['content'].strip()).replace(",",""))
 #     return floated
-
-import re
 
 def string_to_float(input_string):
     # Use regular expressions to find all floating-point numbers and integers
@@ -160,8 +139,7 @@ def calculate_doa(total_debt, total_asset):
 def is_shariah_compliant(coa, doa):
     return coa < 33.0 and doa < 33.0
 
-import streamlit as st
-import plotly.graph_objects as go
+
 
 def generate_ring_pie_chart(chart_name, percentage):
     # Ensure percentage is between 0 and 100
@@ -178,6 +156,28 @@ def generate_ring_pie_chart(chart_name, percentage):
 
     # Display the chart using Streamlit
     st.plotly_chart(fig)
+
+def prefix_converter(text):
+    response = openai.ChatCompletion.create(
+        model="ft:gpt-3.5-turbo-0125:personal::98TxxR5J",  # You can use any appropriate GPT model here
+        messages=[
+            {"role": "system",
+             "content": "please identify the prefix used in this financial report, etc million or RM'000, and convert it into float so that it can be used as a multiplier, I will store it as an float. If no multiplier is present, assign 1.0 ."},
+            {"role": "user", "content": text},
+        ],
+        max_tokens=4000,
+    )
+    multiplier = response['choices'][0]['message']['content'].strip()
+    numbers = re.findall(r'\b\d+(?:\.\d+)?\b', multiplier)
+
+    # Convert numeric substrings to actual floats
+    float_values = [float(num) for num in numbers]
+
+    # If float values are found, return the last float
+    if float_values:
+        return float_values[-1]
+    return 1.0 # default 1.0
+
 
 
 
@@ -198,33 +198,57 @@ def main():
 "STATEMENTS OF FINANCIAL POSITION", "BALANCE SHEETS", "CONSOLIDATED STATEMENT OF FINANCIAL POSITION"]
             extracted_text = extract_pages_with_keywords(file_path, keywords)  # Pass file path instead of content
 
+            multiplier = prefix_converter(extracted_text)
+            st.write(multiplier)
+
             # Extract total assets using GPT
             total_assets_str = extract_total_assets(extracted_text)
-            st.write(f" {total_assets_str}")
+            #st.write(f" {total_assets_str}")
 
             total_cash_str = extract_total_cash(extracted_text)
-            st.write(f" {total_cash_str}")
+            #st.write(f" {total_cash_str}")
 
             total_debt_str = extract_total_debt(extracted_text)
-            st.write(f" {total_debt_str}")
+            #st.write(f" {total_debt_str}")
 
-            total_assets = string_to_float(total_assets_str)
-            st.write("Total assets: " + str(total_assets))
+            total_assets = string_to_float(total_assets_str) * multiplier
+            st.write("Total assets: RM" + str(total_assets))
 
-            total_cash = string_to_float(total_cash_str)
-            st.write("Total cash: " + str(total_cash))
+            total_cash = string_to_float(total_cash_str) * multiplier
+            st.write("Total cash: RM" + str(total_cash))
 
-            total_debt = string_to_float(total_debt_str)
-            st.write("Total Debt: " + str(total_debt))
+            total_debt = string_to_float(total_debt_str) * multiplier
+            st.write("Total Debt: RM" + str(total_debt))
 
-            COA = (total_cash / total_assets)*100
-            DOA = (total_debt / total_assets)*100
+            COA = round((total_cash / total_assets)*100, 2)
+            DOA = round((total_debt / total_assets)*100, 2)
 
             st.write("COA: {:.2f}%".format(COA))
             generate_ring_pie_chart("Cash over Asset Percentage Chart", COA)
 
             st.write("DOA: {:.2f}%".format(DOA))
             generate_ring_pie_chart("Debt over Asset Percentage Chart", DOA)
+
+            # Visualizing COA and DOA using a bar chart
+            metrics = ['COA', 'DOA']
+            values = [COA, DOA]
+            threshold = 33.0
+
+            fig, ax = plt.subplots()
+            ax.bar(metrics, values, color=['blue', 'orange'])
+
+             # Adding a horizontal line for the 33% threshold
+            ax.axhline(y=threshold, color='r', linestyle='--', label='Shariah Compliance Threshold (33%)')
+            ax.legend()
+
+            ax.set_ylabel('Percentage')
+            ax.set_title('Cash and Debt Over Assets (COA & DOA)')
+
+            # Adding percentage values on top of each bar
+            for i, value in enumerate(values):
+                ax.text(i, value + 0.5, f"{value}%", ha='center', va='bottom')
+
+            st.pyplot(fig)
 
             syariah_compliant_1 = is_shariah_compliant(COA, DOA)
             if syariah_compliant_1:
@@ -236,10 +260,15 @@ def main():
 
             if st.button("Appeal"):
                 with st.form("input_form"):
+
+                    total_asset_old = total_assets
+                    total_cash_old = total_cash
+                    total_debt_old = total_debt
+
                     st.write("Enter correct values if there are any errors:")
-                    total_asset = st.number_input("Total Asset", value=1000000)
-                    total_debt = st.number_input("Total Debt", value=500000)
-                    total_cash = st.number_input("Total Cash", value=200000)
+                    total_asset = st.number_input("Total Asset", value=total_asset_old)
+                    total_debt = st.number_input("Total Debt", value=total_cash_old)
+                    total_cash = st.number_input("Total Cash", value=total_debt_old)
 
                     submit_button = st.form_submit_button(label='Submit')
 
